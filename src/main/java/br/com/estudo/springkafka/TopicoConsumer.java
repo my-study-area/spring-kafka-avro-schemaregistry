7845a3d3e894d8040a402767b423554a950741ae
Adriano Avelino <adriano-st777@hotmail.com>
2023-11-19 16:52:41 -0300

    atualiza serializer

--- src/main/java/br/com/estudo/springkafka/TopicoConsumer.java
+++ src/main/java/br/com/estudo/springkafka/TopicoConsumer.java
@@ -5,7 +5,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.util.Objects;
 
 @EnableKafka
 @Component
@@ -13,7 +17,10 @@ public class TopicoConsumer {
     Logger logger = LoggerFactory.getLogger(TopicoConsumer.class);
 
     @KafkaListener(topics = "${topic.name.producer.avro}", groupId = "${spring.kafka.consumer.group-id}")
-    public void listen(ConsumerRecord<String, Mensagem> in) {
+    public void listen(ConsumerRecord<String, Mensagem> in,
+                       @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY) Object object) {
+
+        logger.info("Key: {}",String.valueOf(object));
         logger.info("Consumer ...");
         logger.info(String.valueOf(in.value()));
     }
--- src/main/java/br/com/estudo/springkafka/TopicoProducer.java
+++ src/main/java/br/com/estudo/springkafka/TopicoProducer.java
@@ -1,10 +1,13 @@
 package br.com.estudo.springkafka;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class TopicoProducer {
@@ -31,8 +34,14 @@ public class TopicoProducer {
     }
 
     public void enviarMensagemAvro(Mensagem mensagem) {
-        kafkaTemplateAvro.send(topicNameAvro, mensagem.getRemetente().toString(), mensagem);
+//        kafkaTemplateAvro.send(topicNameAvro, mensagem.getRemetente().toString(), mensagem);
+        String key = UUID.randomUUID().toString();
+        ProducerRecord<String, Mensagem> producerRecord = new ProducerRecord<>(key, mensagem);
+//        kafkaTemplateAvro.send(producerRecord);
+        kafkaTemplateAvro.setDefaultTopic(topicNameAvro);
+        kafkaTemplateAvro.sendDefault(key, mensagem);
         logger.info("Mensagem {} enviada", mensagem);
+        logger.info("ProducerRecord enviado: {}", producerRecord);
     }
 
 }
