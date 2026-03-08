5a8801ed578583d58b6c41ef9a116db3c32b6188
Adriano Avelino <adriano-st777@hotmail.com>
2026-03-08 00:16:27 -0300

    feat: initial project setup with hexagonal architecture

---
+++ src/main/java/com/company/gravity/domain/model/Greeting.java
@@ -0,0 +1,10 @@
+package com.company.gravity.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Greeting {
+    private final String message;
+}
